5ce32f78d83066d695e16f6e542ddcdc397b2ad4
Hamid Abdulmalik <hamidabdulmalik@gmail.com>
2020-06-14 01:44:42 +0200

    Created Quizzes.js and modified Questions.js

---
+++ src/api/Quizzes.js
@@ -0,0 +1,19 @@
+import InitApi from "./InitApi";
+
+export const saveNewQuiz = newQuiz => {
+    const Quizzes = InitApi.parse.Object.extend("Quizzes");
+    const quiz = new Quizzes();
+    return quiz.save(newQuiz);
+};
+
+export const getQuezzes = () => {
+    const Quizzes = InitApi.parse.Object.extend("Quizzes");
+    const quizzes = new InitApi.parse.Query(Quizzes);
+    return quizzes.limit(500).find();
+};
+
+export const quizzesLiveQuery = () => {
+    let query = initApi.parse.Query("Quizzes");
+    let subscription = initApi.client.subscribe(query);
+    return subscription;
+};
