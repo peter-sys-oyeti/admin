83729ba94ff6d2571222aaa1d988b1196d2f6b31
Hamid Abdulmalik <hamidabdulmalik@gmail.com>
2020-06-13 16:52:29 +0200

    created AnswersStore.js

---
+++ src/stores/AnswersStore.js
@@ -0,0 +1,17 @@
+import { types } from "mobx-state-tree";
+
+export const Answer = types.model("Answer", {
+    id: types.string,
+    questionId: types.string,
+    answer: types.string
+});
+
+export const AnswersStore = types
+    .model("AnswersStore", {
+        answers: types.array(Answer)
+    })
+    .actions(self => ({
+        addAnswer(answer) {
+            self.answers.push(answer);
+        }
+    }));
