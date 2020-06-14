44be932a75cb1266e7520c0bbfd6f2e42186c7a1
Hamid Abdulmalik <hamidabdulmalik@gmail.com>
2020-06-14 11:40:08 +0200

    imported libraries RootStore, Provider and BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './components/App'
+import React from "react";
+import { render } from "react-dom";
+import App from "./components/App";
 
+import { RootStore } from "./stores/RootStore";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "mobx-react";
 // Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
-let root = document.createElement('div')
+let root = document.createElement("div");
 
-root.id = 'root'
-document.body.appendChild(root)
+root.id = "root";
+document.body.appendChild(root);
 
 // Now we can render our application into it
-render(<App />, document.getElementById('root'))
+render(<App />, document.getElementById("root"));
