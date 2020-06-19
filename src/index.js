5ff4e841257a7bcde9c1c4a24faf5f7121ec1799
Hamid Abdulmalik <hamidabdulmalik@gmail.com>
2020-06-19 18:05:13 +0200

    basic functions are working

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
-import React from "react";
-import { render } from "react-dom";
-import App from "./components/App";
-import { RootStore } from "./stores/RootStore";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "mobx-react";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
+import {RootStore} from './stores/RootStore';
+import {HashRouter} from 'react-router-dom';
+import {Provider} from 'mobx-react';
 import { bootStore } from "./Bootstrapper";
-let head = document.getElementsByTagName("head")[0];
-let style = document.createElement("link");
+import App from "./components/App";
 
-style.href = "//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css";
-style.type = "text/css";
-style.rel = "stylesheet";
-head.appendChild(style);
-
-let root = document.createElement("div");
-root.id = "root";
-document.body.appendChild(root);
+import 'semantic-ui-css/semantic.min.css';
 
 const store = RootStore.create({});
 bootStore(store);
-render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById("root")
+
+ReactDOM.render(
+  <Provider store={store}>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </Provider>,
+  document.getElementById('root'),
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
